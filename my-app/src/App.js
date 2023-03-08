b1ecdf97ef0ad5d7f88bbe271c82110171332205
Thomas Szatmary <u7303252@anu.edu.au>
2023-03-08 18:02:07 +1100

    1:29h

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -9,15 +9,18 @@ import Button from "./components/Button";
 import "./index.css"
 import ModeToggler from './components/ModeToggler';
 import Promo from './components/Promo';
+import Child from './components/Child';
 
 
 export function App(props) {
+  const date = new Date();
   return (
     <>
       <Nav first="Home" second="Articles" third="About" fourth="Contact"/>
       <Header name="Anna" color="purple"/>
       <Promo/>
       <Main/>
+      <Child message={date.toLocaleTimeString()}/>
       <Logo test="pic"/>
       <ModeToggler />
       <Button />
