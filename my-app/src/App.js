5029c8a023385a3aa3eecc24f6503792ec2deacb
Thomas Szatmary <u7303252@anu.edu.au>
2023-03-08 18:12:47 +1100

    Clock 1:33h

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,19 +7,17 @@ import "./index.css"
 import ModeToggler from './components/ModeToggler';
 import Promo from './components/Promo';
 import Child from './components/Child';
-import { useState } from 'react';
 
 
 export function App(props) {
   const date = new Date();
-  const[showMenu, setShowMenu] = useState(false);
   return (
     <>
       <Nav first="Home" second="Articles" third="About" fourth="Contact"/>
       <Header name="Anna" color="purple"/>
       <Promo/>
       <Main/>
-      <Child message={date.toLocaleTimeString()} output={showMenu} run={setShowMenu(false)}/>
+      <Child message={date.toLocaleTimeString()}/>
       <Logo test="pic"/>
       <ModeToggler />
       <Button />
---
+++ my-app/src/components/Clock.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Clock(props) { 
+    return(
+        <h1>{props.status}</h1>
+    )
+}
+
+export default Clock;
