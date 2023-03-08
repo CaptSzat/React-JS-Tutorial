ae26d5a0d42124770da45b734f23656a41a09caf
Thomas Szatmary <u7303252@anu.edu.au>
2023-03-08 12:20:13 +1100

    Events 1:09h

--- my-app/src/components/Button.js
+++ my-app/src/components/Button.js
@@ -1,16 +1,30 @@
+// import React from "react";
+
+// function addCart(cartNumber){
+//     cartNumber += 1;
+//     return cartNumber;
+// }
+// let cartAmount = 0;
+
+// function Button(){
+//     const clickHandler = () => console.log('clicked')
+//     return(
+//         <div>
+//             <button onClick={clickHandler}>Press Me!</button>
+//             <p>{cartAmount}</p>
+//         </div>
+//     )
+// }
+
+// export default Button;
+
 import React from "react";
 
-function addCart(cartNumber){
-    cartNumber += 1;
-    return cartNumber;
-}
-let cartAmount = 0;
-
 function Button(){
+    const clickHandler = () => console.log('mouseOver')
     return(
         <div>
-            <button onClick={addCart(cartAmount)}>Press Me!</button>
-            <p>{cartAmount}</p>
+            <button onMouseOver={clickHandler}>Press Me!</button>
         </div>
     )
 }
